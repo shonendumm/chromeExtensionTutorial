c986791da766fe1e5e0158cbebe05cce90bce742
Soo Hian <shonendumm@gmail.com>
2020-02-01 14:58:48 +0800

    send message from tab/page to background

--- background.js
+++ background.js
@@ -10,3 +10,12 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
   });
 });
+
+// Listens for message from content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "open_new_tab" ) {
+      chrome.tabs.create({"url": request.url});
+    }
+  }
+);
--- content.js
+++ content.js
@@ -5,9 +5,10 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
+      var firstHref = $("a[href^='http']").eq(0).attr("href"); //select and read 1st link
 
       alert(firstHref);
+      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
     }
   }
 );
